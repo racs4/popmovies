e9841c6aafd6fd82404d540494d829f93bdf4d1f
Rheidner <rheidner.achiles@gmail.com>
2020-07-27 15:16:22 -0300

    feat: add link to all moviecards

--- src/widgets/MovieCard.js
+++ src/widgets/MovieCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './MovieCard.css';
 import { IMAGE_URL } from '../config';
+import { Link } from 'react-router-dom';
 
 const img = (path, title) => {
     return path !== null ? 
@@ -13,15 +14,17 @@ const MovieCard = (props) => {
     let detailActive = props.detail ? "detail-active" : "";
     let isThereImg = props.movie.poster_path !== null;
     return (
-        <div className={`card bg-dark text-white movie-card ${detailActive} ${isThereImg ? "" : "detail"}`}>
-            {img(props.movie.poster_path, props.movie.title)}
-            <div className="card-img-overlay">
-                <h5 className={`card-title movie-card-title ${isThereImg ? "detail" : ""} `} > {props.movie.title} </h5> 
-                <p className={`card-text movie-card-text detail `} >{props.movie.overview}</p> 
-                <span id="movie-year" className="card-text">{props.movie.release_date ? props.movie.release_date.split("-")[0] : ""} </span>
-                <span id="movie-rate"> {props.movie.vote_average} </span>                
+        <Link to={`/movie/${props.movie.id}`} >
+            <div className={`card bg-dark text-white movie-card ${detailActive} ${isThereImg ? "" : "detail"}`}>
+                {img(props.movie.poster_path, props.movie.title)}
+                <div className="card-img-overlay">
+                    <h5 className={`card-title movie-card-title ${isThereImg ? "detail" : ""} `} > {props.movie.title} </h5> 
+                    <p className={`card-text movie-card-text detail `} >{props.movie.overview}</p> 
+                    <span id="movie-year" className="card-text">{props.movie.release_date ? props.movie.release_date.split("-")[0] : ""} </span>
+                    <span id="movie-rate"> {props.movie.vote_average} </span>                
+                </div>
             </div>
-        </div>
+        </Link>
     );
 }
 
