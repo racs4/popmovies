a9b90ef0d5c2ece2a606dedef98536b77b4fd135
Rheidner <rheidner.achiles@gmail.com>
2020-07-27 22:51:43 -0300

    feat: change language to english

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ export const API_KEY = 'ddc1aa3e443d4476203cf7f4d1caa49a';
 export const API_URL = 'https://api.themoviedb.org/3';
 export const IMAGE_URL = 'https://image.tmdb.org/t/p';
 export const REGION = 'BR';
-export const LANGUAGE = 'pt_BR';
+export const LANGUAGE = 'en_US';
 export const API_PARAMETERS = `api_key=${API_KEY}&region=${REGION}&language=${LANGUAGE}`;
